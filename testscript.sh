ff103b6a6ac72fae69f3cfa919a15e4712075de9
Hemkumar <jhemkumar@gmail.com>
2018-12-22 16:43:42 +0800

    test hook

--- testscript.sh
+++ testscript.sh
@@ -2,5 +2,6 @@ echo "This is script is to validate testing scm jenkins config"
 ADDITION=$(($VALUE1 + $VALUE2))
 echo "Addtion of $VALUE1 + $VALUE2 = $ADDITION"
 SUBTRACTION=$(($VALUE1 - $VALUE2))
+echo "Commit test.."
 echo "Subtraction of $VALUE1 - $VALUE2 = $SUBTRACTION"
 echo "Script execution completed"
