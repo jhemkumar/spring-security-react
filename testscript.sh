a3d64ea12a136b5fd9f7cfec0aa74e130f7e9fc6
Hemkumar <jhemkumar@gmail.com>
2018-12-22 17:01:04 +0800

    multiply

--- testscript.sh
+++ testscript.sh
@@ -3,4 +3,6 @@ ADDITION=$(($VALUE1 + $VALUE2))
 echo "Addtion of $VALUE1 + $VALUE2 = $ADDITION"
 SUBTRACTION=$(($VALUE1 - $VALUE2))
 echo "Subtraction of $VALUE1 - $VALUE2 = $SUBTRACTION"
+MULTIPLY=$(($VALUE1 - $VALUE2))
+echo "MULTIPLY of $VALUE1 - $VALUE2 = $MULTIPLY"
 echo "Script execution completed"
